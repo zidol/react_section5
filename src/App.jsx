861f6bb35e5b2c397707ae102f6bd32909626031
zidol <ajh3166@naver.com>
2025-07-20 15:45:03 +0900

    Feat : react 컴포넌트 실습

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import "./App.css";
-
-//함수 이름 시작은 무조건 대문자
-const Header = () => {
-  return (
-    <header>
-      <h1>header</h1>
-    </header>
-  );
-};
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <h1>안녕 리액트!</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+//함수 이름 시작은 무조건 대문자
+const Header = () => {
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  );
+};
+
+export default Header;
