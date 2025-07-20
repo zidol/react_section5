c02b6fb3a56297a2cadd01e6db88fa557c61906d
zidol <ajh3166@naver.com>
2025-07-20 15:11:40 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+//함수 이름 시작은 무조건 대문자
+const Header = () => {
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  );
+};
+
+function App() {
+  return (
+    <>
+      <h1>안녕 리액트!</h1>
+    </>
+  );
+}
+
+export default App;
