31d927ea09c2837fe42e53ce6b4ee6add64a2351
zidol <ajh3166@naver.com>
2025-08-08 23:39:20 +0900

    Feat : React Hooks example

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 
 import Register from "./components/Register";
+import HookExam from "./components/HookExam";
 
 function App() {
   return (
     <>
-      <Register />
+      <HookExam />
     </>
   );
 }
